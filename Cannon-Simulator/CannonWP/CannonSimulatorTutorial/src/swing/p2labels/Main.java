41704236f3de6395db4b6fbf4de0a1bbea343344
abhijeet1069 <abhijeet1069@gmail.com>
2023-08-21 21:02:07 +0530

    new commit

--- Cannon-Simulator/CannonWP/CannonSimulatorTutorial/src/swing/p2labels/Main.java
+++ Cannon-Simulator/CannonWP/CannonSimulatorTutorial/src/swing/p2labels/Main.java
@@ -1,17 +1,28 @@
 package swing.p2labels;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
+		// JLabel = a GUI display area for a string of text, an image or both
+		ImageIcon image = new ImageIcon("/Users/satyam/Personal/OpenSource/Cannon-Simulator/CannonWP/CannonSimulatorTutorial/res/among_us.png");
+		
+		JLabel label = new JLabel();
+		label.setText("Bro, do you even code?");
+		label.setIcon(image);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalTextPosition(JLabel.TOP);
+		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500,500);
+		frame.add(label);
+		frame.setSize(800,600);
 		frame.setVisible(true);
 		
-		// JLabel = a GUI display area for a string of text, an image or both
 	}
 
 }
